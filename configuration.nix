136a992e232c1059461513d841e028146f4ce292
nick grayson <intercomman14@gmail.com>
2024-07-26 16:33:39 -0700

    49 current  2024-07-26 16:27:31  24.05.3164.63d37ccd2d17  6.6.41                          *

--- configuration.nix
+++ configuration.nix
@@ -42,8 +42,6 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-  #  Enable the KDE Plasma Desktop Environment.
-  services.desktopManager.plasma6.enable = true;
 
   services.xserver = {
     layout = "us";
