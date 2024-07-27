6e6f4620890f89195a707287ec652e362a7127c5
nick grayson <intercomman14@gmail.com>
2024-07-27 01:09:59 -0700

    58 current  2024-07-27 01:09:54  24.05.3164.63d37ccd2d17  6.6.41                          *

--- configuration.nix
+++ configuration.nix
@@ -216,7 +216,7 @@
     brightnessctl
   ];
 
-  services.gnome.gnome-keyring.enable = true;
+  # services.gnome.gnome-keyring.enable = true;
 
   fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["FiraCode" "DroidSansMono" "Iosevka"];})
