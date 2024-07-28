65d7456b3b1ef905095019e2e227fbc7f3766948
nick grayson <intercomman14@gmail.com>
2024-07-27 18:56:38 -0700

    58 current  2024-07-27 01:09:54  24.05.3164.63d37ccd2d17  6.6.41                          *

--- configuration.nix
+++ configuration.nix
@@ -73,7 +73,8 @@
   services.printing.enable = true;
 
   # Enable Bluetooth
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
   # Enable polkit
   security.polkit.enable = true;
