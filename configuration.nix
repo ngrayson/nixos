6fffcfb5b76bbd5771d9b269de77a57f0f9766ae
nick grayson <intercomman14@gmail.com>
2024-09-30 21:51:27 -0700

    81 current  2024-09-30 21:51:02  24.05.3346.8c5066250910  6.6.42                          *

--- configuration.nix
+++ configuration.nix
@@ -138,7 +138,12 @@
   # Install firefox.
   programs.firefox.enable = true;
 
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   environment = {
     shells = [pkgs.zsh];
@@ -235,6 +240,8 @@
     #swww
     #alacritty
     brightnessctl
+    # gamer hours
+    steam
   ];
 
   # services.gnome.gnome-keyring.enable = true;
